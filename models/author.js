920d0bfc8789c762c21afd82546bb60ea4e2513f
HARSH-SHETH <ycdtharsh@gmail.com>
2020-08-06 16:44:24 +0530

    author model

---
+++ models/author.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var Scehma = mongoose.Scehma;
+
+var AuthorSchema = new Schema({
+  first_name: {type: String, required: true, maxlength: 100},
+  family_name: {type: String, required: true, maxlength: 100},
+  date_of_birth: {type: Date}, 
+  date_of_death: {type: Date},
+});
+
+// VIRTUAL FOR AUTHOR'S FULL NAME
+AuthorSchema.virtual('name').get(function(){
+  var fullname = '';
+  if(this.first_name && this.family_name){
+    fullname = this.first_name + ', ' + this.family_name;
+  }
+  return fullname;
+});
+
+// VIRTUAL FOR AUTHOR'S LIFESPAN
+AuthorSchema.virtual('lifespan').get(function(){
+  return (this.date_of_birth.getYear() - this.date_of_death.getYear()).toString();
+});
+
+// VIRTUAL FOR AUTHOR'S URL 
+AuthorSchema.virtual('url').get(function(){
+  return "/catalog/author/" + this._id;
+});
+
+// EXPORT MODULE
+module.exports = mongoose.model('Author', AuthorSchema);
