e76a5beaaf537ac5057042f0494cf18bf42ae4ed
HARSH-SHETH <ycdtharsh@gmail.com>
2020-08-06 19:51:44 +0530

    book model

---
+++ models/book.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Scehma;
+
+var BookSchema = new Schema({
+  title: {type: String, required: true}, 
+  author: {type: Schema.Types.ObjectId, ref: 'Author', required: true}, 
+  summary: {type: String, required: true}, 
+  genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+});
+
+// VIRTUAL FOR BOOK'S URL
+BookSchema.virtual('url').get(function(){
+  return '/catalog/book/' + this._id;
+});
